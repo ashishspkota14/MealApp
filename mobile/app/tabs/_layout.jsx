37ceb4161d20b625c9243903066f83cc44c7b005
Aashish <ashishsapkota14@gmail.com>
2026-01-12 12:48:02 -0700

    updated sign in page

---
+++ mobile/app/tabs/_layout.jsx
@@ -0,0 +1,9 @@
+import { Stack , Redirect} from "expo-router"
+import { useAuth } from "@clerk/clerk-expo";
+
+const TabsLayout = () =>{
+    const {isSignedIn} = useAuth()
+    if (!isSignedIn) return <Redirect href={'/(auth)/sign-in'} />
+    return <Stack />
+}
+export default TabsLayout;
