eacdd1a1525f92fc9a8b09f89c38ebfa0049e06e
Aashish <ashishsapkota14@gmail.com>
2026-01-13 17:55:45 -0700

    tabs updated but design left

--- mobile/app/tabs/_layout.jsx
+++ mobile/app/tabs/_layout.jsx
@@ -1,9 +1,40 @@
-import { Stack , Redirect} from "expo-router"
+import { Redirect, Tabs } from "expo-router";
 import { useAuth } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
 
-const TabsLayout = () =>{
-    const {isSignedIn} = useAuth()
-    if (!isSignedIn) return <Redirect href={'/(auth)/sign-in'} />
-    return <Stack />
-}
+const TabsLayout = () => {
+  const { isSignedIn } = useAuth();
+  if (!isSignedIn) return <Redirect href={"/(auth)/sign-in"} />;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Recipes",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="restaurant" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="favorites"
+        options={{
+          title: "Favorites",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="heart" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
 export default TabsLayout;
